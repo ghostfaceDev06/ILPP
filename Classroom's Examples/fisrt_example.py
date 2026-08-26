9e2ca8a396a385b9f326d024baf0c445479914f6
ghostface <raphaelmeirelles240@gmail.com>
2026-08-26 09:55:07 -0300

    Refactor first_example.py with improved comments
    Added comments for clarity and corrected minor typos.

--- Classroom's Examples/fisrt_example.py
+++ Classroom's Examples/fisrt_example.py
@@ -1,9 +1,9 @@
 import gurobipy as gb #Module of Linear Programming Problem
 from gurobipy import GRB
 """ First class about Inter Linear Programming"""
-c = [2,3,2,5] 
-a = [1,1,2,3] 
-cap = 3
+c = [2,3,2,5] #costs 
+a = [1,1,2,3] # item's capacity 
+cap = 3 # bag's capacity
 
 #Create a New Model
 model = gb.Model('First')
@@ -28,4 +28,4 @@ if model.status == GRB.OPTIMAL:
     print(f'Optimal objective Value: {model.ObjVal}')
     print(f'Solve: x1 = {x1.X} \t x2 = {x2.X} \t x3 = {x3.X} \t x4 = {x4.X} ')
 
-'''This examples I learn how to devlop with Gurobipy Pyhton Library to solve Inter Linear Programming´s Problems '''
+'''This examples I learn how to devlop with Gurobipy Pyhton Library to solve Inter Linear Programming´s Problems, in special the bag´s problem '''
